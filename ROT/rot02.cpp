8a26b4ba26dc7d8a2eca8a577b0f7d4a9a00cda6
deuteri <hoangdinhhieupk@gmail.com>
2021-07-28 15:45:39 +0700

    change name

---
+++ ROT/rot02.cpp
@@ -0,0 +1,73 @@
+#include"library.h"
+
+// Function to encrypt the string
+string encrypt(string message){
+    string cipher = "";
+    for(int i = 0; i < message.size(); i++){
+
+        if (message[i] != 32){ // Check for duplicates between message[i] and backspace key        
+            
+            if (message[i] >= UPPER_A && message[i] < UPPER_Y)
+                cipher += message[i] + 2;
+            else if (message[i] >= UPPER_Y && message[i] <= UPPER_Z)
+                cipher += message[i] - 24;
+            else if (message[i] >= LOWER_A && message[i] < LOWER_Y)
+                cipher += message[i] + 2;
+            else if (message[i] >= LOWER_Y && message[i] <= LOWER_Z)
+                cipher += message[i] - 24;
+            else if(message[i] < UPPER_A || message[i] > LOWER_Z || (message[i] > UPPER_Z && message[i] < LOWER_A))
+                cipher += message[i];
+            
+        }
+
+        else 
+            // Add backspace key
+            cipher += " ";
+    }
+
+    return cipher;
+}
+
+// Function to decrypt the string
+string decrypt(string message){
+    string decipher = "";
+    int index = 0;
+    for(int i = 0; i < message.size(); i++){
+
+        if (message[i] != 32){ // Check message[i] backspace key
+            if (message[i] > UPPER_B && message[i] <= UPPER_Z)
+                decipher += message[i] - 2;
+            else if (message[i] <= UPPER_B && message[i] >= UPPER_A)
+                decipher += message[i] + 24;
+
+            else if (message[i] > LOWER_B && message[i] <= LOWER_Z)
+                decipher += message[i] - 2;
+            else if (message[i] <= LOWER_B && message[i] >= LOWER_A)
+                decipher += message[i] + 24;
+
+            else if(message[i] < UPPER_A || message[i] > LOWER_Z || (message[i] > UPPER_Z && message[i] < LOWER_A))
+                decipher += message[i];
+        }
+
+        else if ( message[i] == 32) 
+            // Add backspace key
+            decipher += " ";
+    }
+
+    return decipher;
+}
+  
+// Driver code
+int main(){
+    cls;
+    string message;
+
+    cout << "Input message: ";
+    getline(cin, message);
+
+    cout << "The message after encrypting: " << encrypt(message) << "\n";
+    cout << "The message after decrypting: " << decrypt(message) << "\n";
+
+    pause;
+    return 0;
+}
---
+++ ROT/rot04.cpp
@@ -0,0 +1,72 @@
+#include"library.h"
+
+// Function to encrypt the string
+string encrypt(string message){
+    string cipher = "";
+    for(int i = 0; i < message.size(); i++){
+
+        if (message[i] != 32){ // Check for duplicates between message[i] and backspace key
+            
+            if (message[i] >= UPPER_A && message[i] < UPPER_W)
+                cipher += message[i] + 4;
+            else if (message[i] >= UPPER_W && message[i] <= UPPER_Z)
+                cipher += message[i] - 22;
+            else if (message[i] >= LOWER_A && message[i] < LOWER_W)
+                cipher += message[i] + 4;
+            else if (message[i] >= LOWER_W && message[i] <= LOWER_Z)
+                cipher += message[i] - 22;
+            else if(message[i] < UPPER_A || message[i] > LOWER_Z || (message[i] > UPPER_Z && message[i] < LOWER_A))
+                cipher += message[i];
+            
+        }
+
+        else 
+            // Add backspace key
+            cipher += " ";
+    }
+
+    return cipher;
+}
+
+// Function to decrypt the string
+string decrypt(string message){
+    string decipher = "";
+    for(int i = 0; i < message.size(); i++){
+
+        if (message[i] != 32){ // Check for duplicates between message[i] and backspace key
+            
+            if (message[i] > UPPER_D && message[i] <= UPPER_Z)
+                decipher += message[i] - 4;
+            else if (message[i] >= UPPER_A && message[i] <= UPPER_D)
+                decipher += message[i] + 22;
+            else if (message[i] > LOWER_D && message[i] <= LOWER_Z)
+                decipher += message[i] - 4;
+            else if (message[i] >= LOWER_A && message[i] <= LOWER_D)
+                decipher += message[i] + 22;
+            else if(message[i] < UPPER_A || message[i] > LOWER_Z || (message[i] > UPPER_Z && message[i] < LOWER_A))
+                decipher += message[i];
+            
+        }
+
+        else 
+            // Add backspace key
+            decipher += " ";
+    }
+
+    return decipher;
+}
+  
+// Driver code
+int main(){
+    cls;
+    string message;
+
+    cout << "Input message: ";
+    getline(cin, message);
+
+    cout << "The message after encrypting: " << encrypt(message) << "\n";
+    cout << "The message after decrypting: " << decrypt(message) << "\n";
+
+    pause;
+    return 0;
+}
